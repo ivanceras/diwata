8fe47b87936a685a18aefe7b7af1b19fe380f3ab
Jovansonlee Cesar <ivanceras@gmail.com>
2017-10-08 21:34:43 +0800

    sample code for working around numeric limitations

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,26 +1,57 @@
 extern crate rustorm;
+#[macro_use]
+extern crate dao_codegen;
+extern crate dao;
+extern crate chrono;
+
 use rustorm::Pool;
 use rustorm::Database;
 use rustorm::Rows;
 use rustorm::Dao;
 use rustorm::DbError;
 
+use chrono::DateTime;
+use chrono::offset::Utc;
+use rustorm::{ToTable, ToColumns, FromDao, ToDao};
+
+
+#[derive(Debug, ToTable, ToColumns, FromDao, ToDao)]
+struct Film{
+    film_id: i32,
+    title: String,
+    description: Option<String>,
+    release_year: Option<i32>,
+    language_id: i16,
+    original_language_id: Option<i16>,
+    rental_duration: i16,
+    rental_rate: f64,//TODO: HACKY: column is casted into f64 since support for numeric is not yet finalized
+    length: Option<i16>,
+    replacement_cost: f64, //TODO: numeric hack
+    //rating: Option<String>,
+    last_update: DateTime<Utc>,
+    //special_features:Vec<String>,
+}
+
 fn main() {
     let mut pool = Pool::new();
-    let db_url = "postgres://postgres:p0stgr3s@localhost/rforum";
-    let db = pool.db(db_url).unwrap();
-    let rows: Result<Rows, DbError> = (&db).execute_sql_with_return(
-        "select now(), 'Hello world'::TEXT,  '9a7a36d0-1010-11e5-a475-33082a4698d6'::UUID",
-        &[],
-    );
-    println!("columns: {:#?}", rows);
-    if let Ok(rows) = rows {
-        for row in rows.iter() {
-            println!("row {:?}", row);
+    let db_url = "postgres://postgres:p0stgr3s@localhost/sakila";
+    let em = pool.em(db_url).unwrap();
+    
+    //let films: Result<Vec<Film>, DbError> = em.get_all();
+    let films: Result<Vec<Film>, DbError> = em.execute_sql_with_return(
+        "SELECT film_id, title, description,
+        release_year, language_id,
+        original_language_id,
+        rental_duration,
+        rental_rate::FLOAT8,
+        length,
+        replacement_cost::FLOAT8,
+        last_update
+        FROM film", &[]);
+    println!("films: {:#?}", films);
+    if let Ok(films) = films{
+        for film in films{
+            println!("{:#?}", film);
         }
     }
-
-    let articles: Result<Rows, DbError> =
-        (&db).execute_sql_with_return("select * from article", &[]);
-    println!("articles: {:#?}", articles);
 }
