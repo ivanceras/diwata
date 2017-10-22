6ad78eacec0941baef8788da1e17ada4ce8ca075
ivanceras <ivanceras@gmail.com>
2017-10-23 07:30:16 +0800

    Add test data for composite foreign key

---
+++ dbscripts/reimport_sakila_postgres.sh
@@ -0,0 +1,5 @@
+
+psql -U postgres -h localhost -d sakila -c "DROP schema public CASCADE; CREATE schema public;"
+psql -U postgres -h localhost -d sakila -f ../sakila/postgres-sakila-db/postgres-sakila-schema.sql 
+psql -U postgres -h localhost -d sakila -f ../sakila/postgres-sakila-db/postgres-sakila-data.sql
+psql -U postgres -h localhost -d sakila -f ./composite_foreign_key.sql
