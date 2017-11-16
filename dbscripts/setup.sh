96556dd432cbd4dbbe165cc8545241e482166584
Jovansonlee Cesar <ivanceras@gmail.com>
2017-11-17 00:14:17 +0800

    Use dbscripts/setup.sh for travis-ci

--- dbscripts/setup.sh
+++ dbscripts/setup.sh
@@ -1,6 +1,6 @@
-  - psql -U postgres -h localhost -c 'CREATE DATABASE sakila;' 
-  - psql -U postgres -h localhost -d sakila -f ./sakila/postgres-sakila-db/postgres-sakila-schema.sql 
-  - psql -U postgres -h localhost -d sakila -f ./sakila/postgres-sakila-db/postgres-sakila-data.sql
-  - psql -U postgres -h localhost -d sakila -f ./dbscripts/add_users.sql
-  - psql -U postgres -h localhost -d sakila -f ./dbscripts/composite_foreign_key.sql
-  - psql -U postgres -h localhost -c "ALTER USER postgres WITH PASSWORD 'p0stgr3s';"
+psql -U postgres -h localhost -c 'CREATE DATABASE sakila;' 
+psql -U postgres -h localhost -d sakila -f ./sakila/postgres-sakila-db/postgres-sakila-schema.sql 
+psql -U postgres -h localhost -d sakila -f ./sakila/postgres-sakila-db/postgres-sakila-data.sql
+psql -U postgres -h localhost -d sakila -f ./dbscripts/add_users.sql
+psql -U postgres -h localhost -d sakila -f ./dbscripts/composite_foreign_key.sql
+psql -U postgres -h localhost -c "ALTER USER postgres WITH PASSWORD 'p0stgr3s';"
