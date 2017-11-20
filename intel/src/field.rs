6d35d31a558034f5bf172a704b952324ad01b6ed
Jovansonlee Cesar <ivanceras@gmail.com>
2017-11-21 06:06:50 +0800

    refactor control widget out of reference into widget

--- intel/src/field.rs
+++ intel/src/field.rs
@@ -6,6 +6,7 @@ use rustorm::types::SqlType;
 use rustorm::column::Capacity;
 use rustorm::types::ArrayType;
 use rustorm::Table;
+use widget::ControlWidget;
 
 
 #[derive(Debug, Serialize, Clone)]
@@ -29,7 +30,7 @@ impl Field{
     /// derive field from supplied column
     pub fn from_column(column: &Column) -> Self {
         let reference = Self::try_derive_reference(column);
-        let control_widget = Self::derive_control_widget(column, &reference);
+        let control_widget = ControlWidget::derive_control_widget(column, &reference);
         Field{
             name: column.name.name.to_string(),
             description: column.comment.clone(),
@@ -46,16 +47,7 @@ impl Field{
     /// the field name will be the table name
     /// it looks up to
     pub fn from_has_one_table(columns: &Vec<&Column>, table: &Table) -> Self {
-        let reference = Reference::TableLookup;
-        let widget = reference.get_widget_fullview();
-        let control_widget = ControlWidget {
-            label: table.name.name.to_string(),
-            widget,
-            dropdown_data: None, // not yet computed here
-            width: 20, // get the average widget of the table record display identifier
-            max_len: None,
-            height: 1,
-        };
+        let control_widget = ControlWidget::from_has_one_table(columns, table);
         let mut columns_comment = String::new();
         for col in columns{
             if let Some(ref comment) = col.comment{
@@ -68,7 +60,7 @@ impl Field{
                             Some(columns_comment)
                          }else{None},
             info: table.comment.to_owned(),
-            reference: Some(reference),
+            reference: Some(Reference::TableLookup),
             control_widget
         }
     }
@@ -276,147 +268,9 @@ impl Field{
         }
     }
 
-    /// derive widget base from column
-    /// reference is derived first then the widget is based
-    /// from the reference
-    fn derive_control_widget(column: &Column, reference: &Option<Reference>) -> ControlWidget {
-        let limit = column.specification.get_limit();
-        let (width, height) = if let Some(ref stat) = column.stat{
-            // wrap at 100 character per line
-            if stat.avg_width > 100 {
-                let width = 100;
-                let height = (stat.avg_width - 1) / 100 + 1;
-                (width, height)
-            }
-            else{
-                (stat.avg_width, 1)
-            }
-        }
-        else{
-            (20, 1)
-        };
-        if let Some(ref reference) = *reference{
-            let widget = reference.get_widget_fullview();
-            ControlWidget{
-                label: column.name.name.to_string(),
-                widget,
-                dropdown_data: None,
-                width, 
-                max_len: limit,
-                height,
-            }
-        }
-        else{
-            ControlWidget{
-                label: column.name.name.to_string(),
-                widget: Widget::Textbox,
-                dropdown_data: None,
-                width,
-                max_len: limit,
-                height,
-            }
-        }
-    }
 }
 
 
-/// contains the widget 
-/// and the dropdown data
-#[derive(Debug, Serialize, Clone)]
-pub struct ControlWidget{
-    // the label of the widget
-    label: String,
-    widget: Widget,
-    /// if the widget is Dropdown, DropdownWithImage, AutoCompleteDropdown
-    /// DialogDropdown, CountryList, CountrListWithFlag
-    dropdown_data: Option<DropdownData>,
-    /// width (character wise) of the widget based on
-    /// average of the database values on this column
-    width: i32,
-    /// if limit is set in column this will warn the user
-    /// if the value is too long
-    max_len: Option<i32>,
-    /// height of the control, character wise
-    /// textbox defaults to 1
-    height: i32,
-}
-
-
-/// a simple downdown list in string
-#[derive(Debug, Serialize, Clone)]
-pub struct DropdownRecord{
-    identifier: String,
-    display: String,
-}
-
-#[derive(Debug, Serialize, Clone)]
-pub struct DropdownList{
-    /// api url for the next page to be loaded
-    api_url: String,
-    /// the selected value of the record
-    selected: Option<DropdownRecord>,
-    /// the selection, autoloads on scroll till reaches the last page
-    selection: Vec<DropdownRecord>,
-    current_page: u32,
-    /// whether or not all the items of the page has been loaded
-    reached_last_page: bool,
-}
-
-#[derive(Debug, Serialize, Clone)]
-pub enum Image{
-    Url(String),
-    DataUrl(String),
-    /// image type, blob
-    Blob(String, Vec<u8>),
-    CssClass(String),
-}
-
-
-#[derive(Debug, Serialize, Clone)]
-pub struct DropdownRecordWithImage{
-    identifier: String,
-    display: String,
-    /// the url image of the record display
-    image: Image,
-}
-
-#[derive(Debug, Serialize, Clone)]
-pub struct DropdownListWithImage{
-    /// api url for the next page to be loaded
-    api_url: String,
-    /// the selected value of the record
-    selected: Option<DropdownRecordWithImage>,
-    /// the selection, autoloads on scroll till reaches the last page
-    selection: Vec<DropdownRecordWithImage>,
-    current_page: u32,
-    /// whether or not all the items of the page has been loaded
-    reached_last_page: bool,
-}
-
-#[derive(Debug, Serialize, Clone)]
-pub struct DropdownListWithAutoComplete{
-    /// api url for the next page to be loaded
-    api_url: String,
-    /// the selected value of the record
-    selected: Option<DropdownRecord>,
-    /// the selection, autoloads on scroll till reaches the last page
-    selection: Vec<DropdownRecord>,
-    current_page: u32,
-    /// whether or not all the items of the page has been loaded
-    reached_last_page: bool,
-}
-
-
-#[derive(Debug, Serialize, Clone)]
-pub enum DropdownData{
-    DropdownList(DropdownList),
-    /// whatever the image shape displayed as is
-    DropdownListWithImage(DropdownListWithImage),
-    /// images in rounded corner
-    DropdownListWithRoundedImage(DropdownListWithImage),
-    DropdownListWithAutoComplete(DropdownListWithAutoComplete),
-}
-    
 
 #[cfg(test)]
 
