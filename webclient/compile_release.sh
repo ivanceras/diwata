2decda5e7c505c3f14606dab85b9601fe3af1d26
Jovansonlee Cesar <ivanceras@gmail.com>
2017-12-10 21:55:15 +0800

    Use the minified version of js in release build

--- webclient/compile_release.sh
+++ webclient/compile_release.sh
@@ -1,3 +1,4 @@
 ./compile.sh
 # this needs gogle-closure-compiler, install via `npm install google-closure-compiler`
 google-closure-compiler-js ../public/diwata.js > ../public/diwata.min.js
+sed -i -- 's/diwata.js/diwata.min.js/g' ../public/index.html 
