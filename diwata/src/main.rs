78d1013be648ae15c9ed8c6e39e28c60c8599554
Jovansonlee Cesar <ivanceras@gmail.com>
2017-12-21 03:23:11 +0800

    Add structopts for parsing CLI arguments

--- diwata/src/main.rs
+++ diwata/src/main.rs
@@ -1,6 +1,20 @@
 #![deny(warnings)]
 extern crate diwata;
+extern crate structopt;
+#[macro_use]
+extern crate structopt_derive;
+
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "diwata", about = "A user friendly database interface")] 
+struct Opt{
+    #[structopt(short = "db", long = "dburl", help = "Database url to connect to")]
+    db_url: Option<String>,
+}
 
 fn main() {
+    let opt = Opt::from_args();
+    println!("opt: {:?}", opt);
     diwata::rocket().launch();
 }
