f3ad046b1d2c98dd0e6e940c7e907311e0bdeb7e
Jovansonlee Cesar <ivanceras@gmail.com>
2018-02-15 17:32:00 +0800

    display errors, reformatted with elm-format@exp

--- webclient/src/Page/Window.elm
+++ webclient/src/Page/Window.elm
@@ -2,49 +2,49 @@ module Page.Window
     exposing
         ( Model
         , Msg(..)
-        , init
-        , update
-        , view
-        , subscriptions
         , calcMainTabSize
         , calcMainWindowSize
         , dropdownPageRequestNeeded
+        , init
+        , subscriptions
+        , update
+        , view
         )
 
 {-| Viewing an individual window.
 -}
 
-import Data.Window as Window exposing (Window)
-import Data.Window.Tab as Tab exposing (TabType(..))
-import Data.Window.Record as Record exposing (Rows, Record, RecordId)
 import Data.Session as Session exposing (Session)
 import Data.User as User exposing (User)
 import Data.UserPhoto as UserPhoto
+import Data.Window as Window exposing (Window)
+import Data.Window.Filter as Filter exposing (Condition)
+import Data.Window.GroupedWindow as GroupedWindow exposing (GroupedWindow, WindowName)
+import Data.Window.Lookup as Lookup exposing (Lookup(..))
+import Data.Window.Record as Record exposing (Record, RecordId, Rows)
+import Data.Window.Tab as Tab exposing (TabType(..))
+import Data.Window.TableName as TableName exposing (TableName)
+import Data.WindowArena as WindowArena exposing (ArenaArg)
 import Date exposing (Date)
 import Date.Format
+import Dict exposing (Dict)
 import Html exposing (..)
 import Html.Attributes exposing (attribute, class, disabled, href, id, placeholder)
 import Html.Events exposing (onClick, onInput, onSubmit)
 import Http
 import Page.Errored as Errored exposing (PageLoadError, pageLoadError)
 import Request.Window
+import Request.Window.Delete
 import Request.Window.Records
 import Route
+import Settings exposing (Settings)
 import Task exposing (Task)
 import Util exposing ((=>), pair, viewIf)
-import Dict exposing (Dict)
 import Views.Errors
 import Views.Page as Page
-import Data.Window.GroupedWindow as GroupedWindow exposing (GroupedWindow, WindowName)
-import Data.Window.TableName as TableName exposing (TableName)
 import Views.Window.Tab as Tab
-import Window as BrowserWindow
-import Data.Window.Lookup as Lookup exposing (Lookup(..))
-import Data.Window.Filter as Filter exposing (Condition)
-import Data.WindowArena as WindowArena exposing (ArenaArg)
-import Settings exposing (Settings)
-import Request.Window.Delete
 import Views.Window.Toolbar as Toolbar
+import Window as BrowserWindow
 
 
 -- MODEL --
@@ -86,9 +86,9 @@ calcMainWindowSize browserSize =
         browserWidth =
             toFloat browserSize.width
     in
-        ( browserWidth - totalWidthDeductions
-        , browserHeight - totalHeightDeductions
-        )
+    ( browserWidth - totalWidthDeductions
+    , browserHeight - totalHeightDeductions
+    )
 
 
 calcMainTabSize : BrowserWindow.Size -> ( Float, Float )
@@ -115,9 +115,9 @@ calcMainTabSize browserSize =
         widthDeductions =
             sideMargins
     in
-        ( mainWindowWidth - widthDeductions
-        , mainWindowHeight - heightDeductions
-        )
+    ( mainWindowWidth - widthDeductions
+    , mainWindowHeight - heightDeductions
+    )
 
 
 init : Settings -> Session -> TableName -> Window -> Maybe ArenaArg -> Task PageLoadError Model
@@ -161,7 +161,7 @@ init settings session tableName window arenaArg =
                 _ =
                     Debug.log ("error in loading window" ++ s) e
             in
-                pageLoadError Page.Other "Window is currently unavailable."
+            pageLoadError Page.Other "Window is currently unavailable."
 
         mainTabTask =
             Task.map4
@@ -174,20 +174,20 @@ init settings session tableName window arenaArg =
                 getTotalRecords
                 |> Task.mapError (handleLoadError "in mainTabTask")
     in
-        Task.map2
-            (\mainTab lookup ->
-                { errors = []
-                , tableName = tableName
-                , mainTab = mainTab
-                , window = window
-                , lookup = lookup
-                , arenaArg = arenaArg
-                , dropdownPageRequestInFlight = False
-                , settings = settings
-                }
-            )
-            mainTabTask
-            loadWindowLookups
+    Task.map2
+        (\mainTab lookup ->
+            { errors = []
+            , tableName = tableName
+            , mainTab = mainTab
+            , window = window
+            , lookup = lookup
+            , arenaArg = arenaArg
+            , dropdownPageRequestInFlight = False
+            , settings = settings
+            }
+        )
+        mainTabTask
+        loadWindowLookups
 
 
 
@@ -200,9 +200,21 @@ view session model =
         tableName =
             model.tableName
     in
-        div []
-            [ viewMainTab model
-            ]
+    div [ class "window-main-tab" ]
+        [ viewErrors model
+        , viewMainTab model
+        ]
+
+
+viewErrors : Model -> Html Msg
+viewErrors model =
+    model.errors
+        |> List.map
+            (\e ->
+                div [ class "error" ]
+                    [ text e ]
+            )
+        |> div [ class "errors" ]
 
 
 viewMainTab : Model -> Html Msg
@@ -211,10 +223,10 @@ viewMainTab model =
         mainTab =
             model.mainTab
     in
-        div [ class "main-tab" ]
-            [ Tab.listView model.lookup mainTab
-                |> Html.map TabMsg
-            ]
+    div [ class "main-tab" ]
+        [ Tab.listView model.lookup mainTab
+            |> Html.map TabMsg
+        ]
 
 
 
@@ -243,102 +255,106 @@ update session msg model =
         mainTableName =
             mainTab.tab.tableName
     in
-        case msg of
-            DismissErrors ->
-                { model | errors = [] } => Cmd.none
+    case msg of
+        DismissErrors ->
+            { model | errors = [] } => Cmd.none
 
-            RecordsDeleted rows ->
-                let
-                    _ =
-                        Debug.log "records deleted: " rows
-                in
-                    model => refreshPage mainTab model
+        RecordsDeleted rows ->
+            let
+                _ =
+                    Debug.log "records deleted: " rows
+            in
+            model => refreshPage mainTab model
 
-            RecordsDeleteError error ->
-                { model | errors = model.errors ++ [ error ] }
-                    => Cmd.none
+        RecordsDeleteError error ->
+            let
+                _ =
+                    Debug.log "error deleting records" error
+            in
+            { model | errors = model.errors ++ [ error ] }
+                => Cmd.none
 
-            CloseWindow ->
-                model => Cmd.none
+        CloseWindow ->
+            model => Cmd.none
 
-            TabMsg (Tab.ToolbarMsg Toolbar.ClickedMainDelete) ->
-                let
-                    selectedCount =
-                        Tab.selectedRowCount model.mainTab
+        TabMsg (Tab.ToolbarMsg Toolbar.ClickedMainDelete) ->
+            let
+                selectedCount =
+                    Tab.selectedRowCount model.mainTab
 
-                    settings =
-                        model.settings
+                settings =
+                    model.settings
 
-                    selectedRecordIdList =
-                        Tab.selectedRows mainTab
-                            |> List.map .recordId
+                selectedRecordIdList =
+                    Tab.selectedRows mainTab
+                        |> List.map .recordId
 
-                    _ =
-                        Debug.log ("Initiating delete on  " ++ toString selectedCount ++ " records") ""
-                in
-                    model => requestDeleteRecords settings mainTableName selectedRecordIdList
+                _ =
+                    Debug.log ("Initiating delete on  " ++ toString selectedCount ++ " records") ""
+            in
+            model => requestDeleteRecords settings mainTableName selectedRecordIdList
 
-            TabMsg (Tab.SearchboxMsg searchbox searchMsg) ->
-                let
-                    ( updatedMainTab, subCmd ) =
-                        Tab.update (Tab.SearchboxMsg searchbox searchMsg) model.mainTab
+        TabMsg (Tab.SearchboxMsg searchbox searchMsg) ->
+            let
+                ( updatedMainTab, subCmd ) =
+                    Tab.update (Tab.SearchboxMsg searchbox searchMsg) model.mainTab
 
-                    tabSearchFilter =
-                        updatedMainTab.searchFilter
+                tabSearchFilter =
+                    updatedMainTab.searchFilter
 
-                    newArenaArg =
-                        case model.arenaArg of
-                            Just arenaArg ->
-                                WindowArena.updateFilter tabSearchFilter arenaArg
-                                    |> Just
+                newArenaArg =
+                    case model.arenaArg of
+                        Just arenaArg ->
+                            WindowArena.updateFilter tabSearchFilter arenaArg
+                                |> Just
 
-                            Nothing ->
-                                Nothing
+                        Nothing ->
+                            Nothing
 
-                    updatedModel =
-                        { model
-                            | mainTab = updatedMainTab
-                            , arenaArg = newArenaArg
-                        }
-                in
-                    updatedModel
-                        => Cmd.batch
-                            [ Cmd.map TabMsg subCmd
-                            , refreshPage updatedMainTab updatedModel
-                            , Route.modifyUrl (Route.WindowArena newArenaArg)
-                            ]
-
-            TabMsg tabMsg ->
-                let
-                    ( newMainTab, subCmd ) =
-                        Tab.update tabMsg model.mainTab
-
-                    ( updatedMainTab, tabCmd ) =
-                        if Tab.pageRequestNeeded newMainTab then
-                            { newMainTab | pageRequestInFlight = True }
-                                => requestNextPage newMainTab model
-                        else
-                            newMainTab => Cmd.none
-                in
-                    { model | mainTab = updatedMainTab }
-                        => Cmd.batch
-                            [ Cmd.map TabMsg subCmd
-                            , tabCmd
-                            ]
-
-            LookupNextPageReceived ( sourceTable, recordList ) ->
-                let
-                    updatedLookup =
-                        Lookup.addPage sourceTable recordList model.lookup
-                in
+                updatedModel =
                     { model
-                        | lookup = updatedLookup
-                        , dropdownPageRequestInFlight = False
+                        | mainTab = updatedMainTab
+                        , arenaArg = newArenaArg
                     }
-                        => Cmd.none
+            in
+            updatedModel
+                => Cmd.batch
+                    [ Cmd.map TabMsg subCmd
+                    , refreshPage updatedMainTab updatedModel
+                    , Route.modifyUrl (Route.WindowArena newArenaArg)
+                    ]
 
-            LookupNextPageErrored e ->
-                Debug.crash "Error loading next page lookup" e
+        TabMsg tabMsg ->
+            let
+                ( newMainTab, subCmd ) =
+                    Tab.update tabMsg model.mainTab
+
+                ( updatedMainTab, tabCmd ) =
+                    if Tab.pageRequestNeeded newMainTab then
+                        { newMainTab | pageRequestInFlight = True }
+                            => requestNextPage newMainTab model
+                    else
+                        newMainTab => Cmd.none
+            in
+            { model | mainTab = updatedMainTab }
+                => Cmd.batch
+                    [ Cmd.map TabMsg subCmd
+                    , tabCmd
+                    ]
+
+        LookupNextPageReceived ( sourceTable, recordList ) ->
+            let
+                updatedLookup =
+                    Lookup.addPage sourceTable recordList model.lookup
+            in
+            { model
+                | lookup = updatedLookup
+                , dropdownPageRequestInFlight = False
+            }
+                => Cmd.none
+
+        LookupNextPageErrored e ->
+            Debug.crash "Error loading next page lookup" e
 
 
 {-|
@@ -347,6 +363,7 @@ update session msg model =
     conditions should be met:
         - there is no currently dropdown page request in flight
         - the lookup data for the specific table hasn't reached the last page
+
 -}
 dropdownPageRequestNeeded : Lookup -> Model -> Maybe TableName
 dropdownPageRequestNeeded lookup model =
@@ -354,10 +371,10 @@ dropdownPageRequestNeeded lookup model =
         sourceTable =
             Tab.dropdownPageRequestNeeded lookup model.mainTab
     in
-        if not model.dropdownPageRequestInFlight then
-            sourceTable
-        else
-            Nothing
+    if not model.dropdownPageRequestInFlight then
+        sourceTable
+    else
+        Nothing
 
 
 refreshPage : Tab.Model -> Model -> Cmd Msg
@@ -391,17 +408,17 @@ refreshPage tab model =
             else
                 Request.Window.Records.listPage model.settings tabPage Nothing tab.tab.tableName
     in
-        request
-            |> Http.toTask
-            |> Task.attempt
-                (\result ->
-                    case result of
-                        Ok rows ->
-                            TabMsg (Tab.RefreshPageReceived rows)
+    request
+        |> Http.toTask
+        |> Task.attempt
+            (\result ->
+                case result of
+                    Ok rows ->
+                        TabMsg (Tab.RefreshPageReceived rows)
 
-                        Err e ->
-                            TabMsg (Tab.RefreshPageError (toString e))
-                )
+                    Err e ->
+                        TabMsg (Tab.RefreshPageError (toString e))
+            )
 
 
 requestNextPage : Tab.Model -> Model -> Cmd Msg
@@ -417,15 +434,15 @@ requestNextPage tab model =
                         filter =
                             arenaArg.filter
                     in
-                        case filter of
-                            Just filter ->
-                                if Dict.isEmpty filter then
-                                    Nothing
-                                else
-                                    Just filter
-
-                            Nothing ->
+                    case filter of
+                        Just filter ->
+                            if Dict.isEmpty filter then
                                 Nothing
+                            else
+                                Just filter
+
+                        Nothing ->
+                            Nothing
 
                 Nothing ->
                     Nothing
@@ -433,22 +450,23 @@ requestNextPage tab model =
         tabPage =
             tab.currentPage + 1
     in
-        Request.Window.Records.listPageWithFilter model.settings tabPage Nothing tab.tab.tableName condition
-            |> Http.toTask
-            |> Task.attempt
-                (\result ->
-                    case result of
-                        Ok rows ->
-                            TabMsg (Tab.NextPageReceived rows)
+    Request.Window.Records.listPageWithFilter model.settings tabPage Nothing tab.tab.tableName condition
+        |> Http.toTask
+        |> Task.attempt
+            (\result ->
+                case result of
+                    Ok rows ->
+                        TabMsg (Tab.NextPageReceived rows)
 
-                        Err e ->
-                            TabMsg (Tab.NextPageError (toString e))
-                )
+                    Err e ->
+                        TabMsg (Tab.NextPageError (toString e))
+            )
 
 
 {-|
 
     Delete records from the main table
+
 -}
 requestDeleteRecords : Settings -> TableName -> List RecordId -> Cmd Msg
 requestDeleteRecords settings tableName recordIds =
