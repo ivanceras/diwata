85b88b981427a60ea79b178544fbf63371b729bf
Jovansonlee Cesar <ivanceras@gmail.com>
2019-04-11 14:02:36 +0800

    Add dota-sql import in dbscripts

--- dbscripts/reimport_sakila_postgres.sh
+++ dbscripts/reimport_sakila_postgres.sh
@@ -4,3 +4,6 @@ psql -U postgres -h localhost -d sakila -f ../sakila/postgres-sakila-db/postgres
 psql -U postgres -h localhost -d sakila -f ../sakila/postgres-sakila-db/postgres-sakila-data.sql
 psql -U postgres -h localhost -d sakila -f ./composite_foreign_key.sql
 psql -U postgres -h localhost -d sakila -f ./add_users.sql
+
+cd ../../../dota-sql/data/
+sh reimport.sh
--- dbscripts/setup.sh
+++ dbscripts/setup.sh
@@ -4,3 +4,6 @@ psql -U postgres -h localhost -d sakila -f ./sakila/postgres-sakila-db/postgres-
 psql -U postgres -h localhost -d sakila -f ./dbscripts/add_users.sql
 psql -U postgres -h localhost -d sakila -f ./dbscripts/composite_foreign_key.sql
 psql -U postgres -h localhost -c "ALTER USER postgres WITH PASSWORD 'p0stgr3s';"
+
+cd ../../dota-sql/data/
+sh reimport.sh
