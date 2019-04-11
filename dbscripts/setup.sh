81679b1260994472bbe12aa7a8047c8e837d30f7
Jovansonlee Cesar <ivanceras@gmail.com>
2019-04-11 21:12:44 +0800

    update from submodules

--- dbscripts/setup.sh
+++ dbscripts/setup.sh
@@ -1,3 +1,4 @@
+psql -U postgres -h localhost -c 'DROP DATABASE sakila;' 
 psql -U postgres -h localhost -c 'CREATE DATABASE sakila;' 
 psql -U postgres -h localhost -d sakila -f ./sakila/postgres-sakila-db/postgres-sakila-schema.sql 
 psql -U postgres -h localhost -d sakila -f ./sakila/postgres-sakila-db/postgres-sakila-data.sql
@@ -5,5 +6,4 @@ psql -U postgres -h localhost -d sakila -f ./dbscripts/add_users.sql
 psql -U postgres -h localhost -d sakila -f ./dbscripts/composite_foreign_key.sql
 psql -U postgres -h localhost -c "ALTER USER postgres WITH PASSWORD 'p0stgr3s';"
 
-cd ../../dota-sql/data/
-sh reimport.sh
+cd ../../dota-sql/data/ && sh reimport.sh && cd -
