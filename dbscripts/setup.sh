3d05f12a5cc0ef87318163b6ce28a30c5b5c458a
Jovansonlee Cesar <ivanceras@gmail.com>
2019-04-11 21:46:15 +0800

    include sakila to clone

--- dbscripts/setup.sh
+++ dbscripts/setup.sh
@@ -1,6 +1,6 @@
-psql -U postgres -h localhost -c 'DROP DATABASE sakila;' 
-psql -U postgres -h localhost -c 'CREATE DATABASE sakila;' 
-psql -U postgres -h localhost -d sakila -f ./sakila/postgres-sakila-db/postgres-sakila-schema.sql 
+psql -U postgres -h localhost -c 'DROP DATABASE sakila;'
+psql -U postgres -h localhost -c 'CREATE DATABASE sakila;'
+psql -U postgres -h localhost -d sakila -f ./sakila/postgres-sakila-db/postgres-sakila-schema.sql
 psql -U postgres -h localhost -d sakila -f ./sakila/postgres-sakila-db/postgres-sakila-data.sql
 psql -U postgres -h localhost -d sakila -f ./dbscripts/add_users.sql
 psql -U postgres -h localhost -d sakila -f ./dbscripts/composite_foreign_key.sql
