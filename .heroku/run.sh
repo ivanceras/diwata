553c1d19693544bd0a7073c655fdb7fd81a717f4
Jovansonlee Cesar <ivanceras@gmail.com>
2019-06-03 13:23:13 +0800

    Using sh buildpack for heroku demo

---
+++ .heroku/run.sh
@@ -0,0 +1,13 @@
+if ! type cargo > /dev/null; then
+    curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain nightly
+fi
+# Install wasm-pack if it isn't installed yet
+if ! type wasm-pack > /dev/null; then
+    cargo install wasm-pack
+fi
+
+if cd crates/webapp && wasm-pack build --target no-modules --release; then
+    cd -
+fi
+
+cargo build --release -p diwata_server
